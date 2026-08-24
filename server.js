0a066b35c74f4df6b274446268a6f4bc2a4cc392
Orgonia <alemy112233@gmail.com>
2026-08-24 23:45:49 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors'); 
+const path = require('path');
+
+const app = express();
+
+app.use(cors()); 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const cloudURI = process.env.MONGODB_URI || 'mongodb+srv://sanzharshoman2_db_user:v3qVlRxrjrUSU9zS@cluster0.wakytga.mongodb.net/?appName=Cluster0';
+
+mongoose.connect(cloudURI)
+  .then(() => console.log('=== УСПЕШНОЕ ПОДКЛЮЧЕНИЕ К ОБЛАЧНОЙ MONGODB ATLAS! ==='))
+  .catch(err => console.error('Ошибка подключения к облаку:', err));
+
+const ResultSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    phone: String,
+    detailedAnswers: Array, 
+    score: Number,          
+    createdAt: { type: Date, default: Date.now }
+});
+
+const Result = mongoose.model('Result', ResultSchema);
+
+const correctAnswers = {
+    1: 2, 2: 3, 3: 0, 4: 1, 5: 1, 6: 0, 7: 1, 8: 0, 9: 1, 10: 2,
+    11: 1, 12: 7, 13: 1, 14: 0, 15: 3, 16: 5, 17: 1, 18: 4, 19: 4, 20: 1
+};
+
+app.post('/api/submit-quiz', async (req, res) => {
+    try {
+        const { name, email, phone, answers } = req.body;
+        let detailedAnswers = [];
+        let score = 0;
+
+        for (let qId in correctAnswers) {
+            const numId = parseInt(qId);
+            const userAnswer = answers[numId];
+            const isCorrect = userAnswer === correctAnswers[numId];
+            if (isCorrect) score++;
+
+            detailedAnswers.push({
+                questionId: numId,
+                theme: numId <= 10 ? "Критическое мышление" : "Математика",
+                userAnswer: userAnswer !== undefined ? userAnswer : null,
+                correctAnswer: correctAnswers[numId],
+                isCorrect: isCorrect
+            });
+        }
+
+        const newResult = new Result({ name, email, phone, detailedAnswers, score });
+        await newResult.save();
+        res.status(200).json({ success: true });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Сервер запущен`));
+module.exports = app;
